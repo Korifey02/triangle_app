9696c912c5d825a488b43e8fcd13a3e71c288c36
Timofey <ikorifey@mail.ru>
2023-12-04 14:40:35 +0300

    меняем бд в нашем проекте, в настройках прописываем данные для соединения с postgresql базой, добавляем соответсвующие зависимости

--- triangle_app/settings.py
+++ triangle_app/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'triangle_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'triangle_app',
+        'USER': 'root',
+        'PASSWORD': '1234567',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
